7b04fd71c720db286812c9207807a52bbbf4cd37
unknown <xrxsanti@gmail.com>
2025-05-26 23:43:37 +0200

    Se agrego la conexion de la base de datos desplegada en MongoDB Atlas en el archivo .env MONGO_URI se corrigieron errores gramaticales

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose= require ('mongoose');
 
 const connectDB=async()=> {
     try{
-        await mongooose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB conectado');
     } catch (error){
         console.error(error);
